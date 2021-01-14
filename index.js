defedaca12336a59ed52d4619ec2df49b85bb9f3
Alie Stone <helloaliestone@gmail.com>
2021-01-14 02:02:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-// Code your solutions in this file
-function countDown(number) {
-    while (number < 0) {
-    console.log (number);
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
-    number -= 1;
+  return thankYouCards
+}
+
+function countDown( countdown ) {
+  while ( countdown > 0 ) {
+    console.log( countdown );
+    countdown -= 1;
+  }
+  console.log( countdown );
 }
